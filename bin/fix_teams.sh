7350f43a422148c21e782aa8e1608002f97245ba
John Krug <john.krug@suse.com>
2025-04-23 11:51:09 +0200

    cut-7a, fix the teams.adoc
    Signed-off-by: John Krug <john.krug@suse.com>

--- bin/fix_teams.sh
+++ bin/fix_teams.sh
@@ -2,4 +2,11 @@
 
 f=/home/jhk/projects/suse/product-docs/stackstate-product-docs/docs/next/modules/en/pages/use/alerting/notifications/channels/teams.adoc
 
-sed -i 's/is created in the channel.*$/is created in the channel.\n\n\.Teams messages for an open and close notification\nimage::k8s\/notifications-teams-example.png[Teams example,75%]/' "$f"
+done_already=$(grep '^image::k8s/notifications-teams-example.png\[Teams example,75%\]' "$f")
+if [ -z "$done_already" ]; then
+  sed -i 's/is created in the channel.*/is created in the channel.\n\n\.Teams messages for an open and close notification\nimage::k8s\/notifications-teams-example.png[Teams example,75%]/gm' "$f"
+fi
+
+sed -i 's/image::k8s\/notifications-teams-webhook-template.png/\nimage::k8s\/notifications-teams-webhook-template.png/' "$f"
+
+sed -i '/^$/N;/^\n$/D' "$f"
